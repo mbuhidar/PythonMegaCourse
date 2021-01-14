948cff96bef020f8e0853bfa634fd9651a7e642d
mbuhidar <mbuhidar@gmail.com>
2021-01-14 08:16:12 -0500

    Add files for Section 14

---
+++ Section14_MySQL/app1_mysql_refactored.py
@@ -0,0 +1,49 @@
+# Section 14 - Interactive English Dictionary using MySQL
+
+import mysql.connector
+from difflib import get_close_matches
+
+con = mysql.connector.connect(
+    user="ardit700_student",
+    password="ardit700_student",
+    host="108.167.140.122",
+    database="ardit700_pm1database"
+)
+
+
+def get_results(word):
+    cursor = con.cursor()
+    query = cursor.execute("SELECT * FROM Dictionary WHERE Expression = '%s'" %word)
+    return cursor.fetchall()
+
+
+def get_all():
+    cursor = con.cursor()
+    query = cursor.execute("SELECT * FROM Dictionary")
+    return cursor.fetchall()
+
+
+def print_results(results):
+    for result in results:
+        print(result[1])
+
+
+word = input("Enter a word: ").lower()
+
+if results := get_results(word): # ':=' in-line assignment; Python 3.8 and up
+    print_results(results)
+else:
+    all_data = get_all()
+    all_expressions = [i for i, j in all_data]
+    close_match = get_close_matches(word, all_expressions, cutoff=0.8)
+    if len(close_match) > 0:
+        yn = input(f"Did you mean {close_match[0]} instead? (y or n): ")
+        if yn == "y":
+            results = get_results(close_match[0])
+            print_results(results)
+        elif yn == "n":
+            print("The word doesn't exist. Please double check it.")
+        else:
+            print("We didn't understand your entry.")
+    else:
+        print("No word found!")
